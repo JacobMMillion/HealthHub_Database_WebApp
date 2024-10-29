eb280ee92ad8f785ed7a73de2564c94ddc625bbf
Jacob <105403630+JacobMMillion@users.noreply.github.com>
2024-10-28 22:30:40 -0400

    Update server.py

--- server.py
+++ server.py
@@ -51,12 +51,13 @@ engine = create_engine(DATABASEURI)
 
 
 # Here we create a test table and insert some values in it
-engine.execute("""DROP TABLE IF EXISTS test;""")
-engine.execute("""CREATE TABLE IF NOT EXISTS test (
-  id serial,
-  name text
-);""")
-engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
+with engine.connect() as connection:
+    connection.execute("""DROP TABLE IF EXISTS test;""")
+    engine.execute("""CREATE TABLE IF NOT EXISTS test (
+      id serial,
+      name text
+    );""")
+    engine.execute("""INSERT INTO test(name) VALUES ('grace hopper'), ('alan turing'), ('ada lovelace');""")
 
 
 
