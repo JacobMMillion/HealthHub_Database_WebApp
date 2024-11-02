67f36dd9bb6efb9ec047cf1f6bd159c8c9fa781c
Jacob <105403630+JacobMMillion@users.noreply.github.com>
2024-11-02 10:26:36 -0400

    Update server.py

--- server.py
+++ server.py
@@ -128,7 +128,7 @@ def index():
   #
   # example of a database query
   #
-  cursor = g.conn.execute("SELECT name FROM test")
+  cursor = g.conn.execute(text("SELECT name FROM test"))
   names = []
   for result in cursor:
     names.append(result['name'])  # can also be accessed using result[0]
